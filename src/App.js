83e73f1e24f8eb8f5e31f23d34e60145d7e75f48
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-06-13 17:45:35 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <div>
+     <h1 className="text-4xl font-bold">Hello Sahil</h1>
+     <h3> Lets go for a <FaBeer />? </h3>
+    </div>  
   );
 }
 
